0c2fc2a65ddb2e51aab63399aea901fc6bbf316f
Eleonora Visnevskyte <eleonora.visnevskyte@calbaptist.edu>
2026-01-24 16:31:51 -0800

    Started work

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- // aaa
- //fsh
+ // ToDO: start something
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
