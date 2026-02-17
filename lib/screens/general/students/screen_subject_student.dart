825e1961975ee39af071bcff9dd0b0ab3ffb14b8
Landon Petriccione <LandonMatthew.Petriccione@calbaptist.edu>
2026-02-17 01:44:55 -0800

    Addition of all screens being used for BAY-min

---
+++ lib/screens/general/students/screen_subject_student.dart
@@ -0,0 +1,68 @@
+// -----------------------------------------------------------------------
+// Filename: screen_alternative.dart
+// Original Author: Dan Grissom
+// Creation Date: 10/31/2024
+// Copyright: (c) 2024 CSC322
+// Description: This file contains the screen for a dummy alternative screen
+//               history screen.
+
+//////////////////////////////////////////////////////////////////////////
+// Imports
+//////////////////////////////////////////////////////////////////////////
+
+// Flutter imports
+import 'dart:async';
+
+// Flutter external package imports
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+// App relative file imports
+import '../../../util/message_display/snackbar.dart';
+
+//////////////////////////////////////////////////////////////////////////
+// StateFUL widget which manages state. Simply initializes the state object.
+//////////////////////////////////////////////////////////////////////////
+class ScreenAlternate extends ConsumerStatefulWidget {
+  static const routeName = '/subject_student';
+
+  @override
+  ConsumerState<ScreenAlternate> createState() => _ScreenAlternateState();
+}
+
+//////////////////////////////////////////////////////////////////////////
+// The actual STATE which is managed by the above widget.
+//////////////////////////////////////////////////////////////////////////
+class _ScreenAlternateState extends ConsumerState<ScreenAlternate> {
+  // The "instance variables" managed in this state
+  bool _isInit = true;
+
+  ////////////////////////////////////////////////////////////////
+  // Runs the following code once upon initialization
+  ////////////////////////////////////////////////////////////////
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      _init();
+      _isInit = false;
+      super.didChangeDependencies();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  ////////////////////////////////////////////////////////////////
+  // Initializes state variables and resources
+  ////////////////////////////////////////////////////////////////
+  Future<void> _init() async {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Blank Screen')),
+    );
+  }
+}
