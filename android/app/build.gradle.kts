f2fb79da4ceebc0c4f8a72ebad7234031e5b28da
Eleonora Visnevskyte <eleonora.visnevskyte@calbaptist.edu>
2026-04-12 13:58:06 -0700

    Implement firebase notifications on the phone when a student gets an invite from a supervisor

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,6 +42,11 @@ android {
     }
 }
 
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-messaging")
+}
+
 flutter {
     source = "../.."
 }
